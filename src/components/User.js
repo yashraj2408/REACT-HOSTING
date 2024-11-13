49ab33ca3bc32160899d1c9c84aea09c4d4cb972
NEMALA YASH RAJ <yashrajnemala@gmail.com>
2024-11-13 14:37:33 +0530

    Update User.js

--- src/components/User.js
+++ src/components/User.js
@@ -4,7 +4,7 @@ export default function User() {
 
     function fun1() {
         console.log(document.getElementsByName("t1")[0].value);
-        axios.get("https://jfsd-spring-hosting-production.up.railway.app/user", {params:{
+        axios.get("https://jfsd-spring-hosting-production-e021.up.railway.app/user", {params:{
             email: document.getElementsByName("t1")[0].value
         }}).then((res)=>{
             console.log(res.data)
